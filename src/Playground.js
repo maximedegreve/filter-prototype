d81845d5a02101abddaeb09e2960b3ec69c3fe34
Maxime De Greve <maximedegreve@github.com>
2023-05-31 22:09:03 +0100

    wup

--- src/Playground.js
+++ src/Playground.js
@@ -1,13 +1,6 @@
 import { useState } from 'react'
-import {
-    Box,
-    Button,
-    StyledOcticon,
-    ActionList,
-    FormControl,
-    Checkbox,
-    CheckboxGroup,
-} from '@primer/react'
+import { Box, Text, FormControl, Checkbox, CheckboxGroup } from '@primer/react'
+import actionMenuImage from './images/actionmenu.png'
 
 const Feature = {
     FocusableContent: 'focusable-content',
@@ -57,7 +50,8 @@ const featureInfo = {
 const components = [
     {
         id: 'hover-cards',
-        question: 'Hover cards',
+        name: 'Hover cards',
+        description: `Vertical list of interactive actions or options. It's composed of items presented in a consistent.`,
         features: [
             Feature.FocusableContent,
             Feature.Contextual,
@@ -67,7 +61,8 @@ const components = [
     },
     {
         id: 'tooltip',
-        question: 'Tooltip',
+        name: 'Tooltip',
+        description: `Vertical list of interactive actions or options. It's composed of items presented in a consistent.`,
         features: [
             Feature.Contextual,
             Feature.TriggerInGlobalNavigation,
@@ -76,7 +71,9 @@ const components = [
     },
     {
         id: 'action-menu',
-        question: 'Action Menu',
+        name: 'Action Menu',
+        image: actionMenuImage,
+        description: `Vertical list of interactive actions or options. It's composed of items presented in a consistent.`,
         features: [
             Feature.FocusableContent,
             Feature.Contextual,
@@ -87,7 +84,8 @@ const components = [
     },
     {
         id: 'select-panel',
-        question: 'Select panel',
+        name: 'Select panel',
+        description: `Vertical list of interactive actions or options. It's composed of items presented in a consistent.`,
         features: [
             Feature.FormElements,
             Feature.FocusableContent,
@@ -98,7 +96,8 @@ const components = [
     },
     {
         id: 'dialog',
-        question: 'Dialog',
+        name: 'Dialog',
+        description: `Vertical list of interactive actions or options. It's composed of items presented in a consistent.`,
         features: [
             Feature.FormElements,
             Feature.FocusableContent,
@@ -110,7 +109,8 @@ const components = [
     },
     {
         id: 'Right sidesheet',
-        question: 'Right sidesheet',
+        name: 'Right sidesheet',
+        description: `Vertical list of interactive actions or options. It's composed of items presented in a consistent.`,
         features: [
             Feature.FocusableContent,
             Feature.Contextual,
@@ -120,7 +120,8 @@ const components = [
     },
     {
         id: 'right-side-panel',
-        question: 'Right sidepanel',
+        name: 'Right sidepanel',
+        description: `Vertical list of interactive actions or options. It's composed of items presented in a consistent.`,
         features: [
             Feature.FormElements,
             Feature.FocusableContent,
@@ -131,7 +132,8 @@ const components = [
     },
     {
         id: 'new-page',
-        question: 'New page',
+        name: 'New page',
+        description: `Vertical list of interactive actions or options. It's composed of items presented in a consistent.`,
         features: [
             Feature.FocusableContent,
             Feature.Contextual,
@@ -146,13 +148,13 @@ const components = [
 
 function Playground() {
     const [selection, setSelection] = useState({
-        [Feature.FocusableContent]: true,
-        [Feature.Contextual]: true,
-        [Feature.TriggerInGlobalNavigation]: true,
-        [Feature.NoOuterRightTriggerLimitation]: true,
-        [Feature.Actionable]: true,
-        [Feature.FormElements]: true,
-        [Feature.DeepLinking]: true,
+        [Feature.FocusableContent]: false,
+        [Feature.Contextual]: false,
+        [Feature.TriggerInGlobalNavigation]: false,
+        [Feature.NoOuterRightTriggerLimitation]: false,
+        [Feature.Actionable]: false,
+        [Feature.FormElements]: false,
+        [Feature.DeepLinking]: false,
     })
 
     const features = Object.keys(selection).map((key) => {
@@ -174,34 +176,133 @@ function Playground() {
     })
 
     let compatibleComponents = []
+    let inCompatibleComponents = []
     components.forEach(function (component) {
         let intersection = component.features.filter((x) =>
             possibleFeatures.includes(x)
         )
         if (intersection.length == possibleFeatures.length) {
             compatibleComponents.push(component)
+        } else {
+            inCompatibleComponents.push(component)
         }
     })
 
-    console.log(compatibleComponents)
     return (
-        <main>
-            <h1>Validate your component usage</h1>
-            <Box>
-                <h2>Requirements checklist</h2>
-
-                <CheckboxGroup>{features}</CheckboxGroup>
-            </Box>
-
-            <Box>
-                <h2>Compatible components</h2>
-                <Box>
-                    {compatibleComponents.map((c) => {
-                        return <Box>{c.question}</Box>
-                    })}
+        <Box
+            as="main"
+            sx={{
+                display: 'flex',
+                p: 5,
+                flexDirection: 'column',
+                bg: '#FAFAFC',
+                minHeight: '100vh',
+            }}
+        >
+            <Text sx={{ fontSize: 7, pb: 4, fontWeight: 'bold' }}>
+                Component picker
+            </Text>
+            <Box
+                sx={{
+                    display: 'flex',
+                }}
+            >
+                <Box sx={{ pr: 5 }}>
+                    <Box sx={{ bg: '#F5F5F7', borderRadius: '30px', p: 5 }}>
+                        <Text
+                            as="h2"
+                            sx={{ fontSize: 1, pb: 3, color: 'fg.subtle' }}
+                        >
+                            Checklist (requirements)
+                        </Text>
+                        <CheckboxGroup>{features}</CheckboxGroup>
+                    </Box>
+                </Box>
+                <Box sx={{ flex: 1 }}>
+                    <Box
+                        sx={{
+                            display: 'grid',
+                            gridTemplateColumns: [
+                                'repeat(1,1fr)',
+                                'repeat(1,1fr)',
+                                'repeat(1,1fr)',
+                                'repeat(2,1fr)',
+                                'repeat(3,1fr)',
+                            ],
+                            gap: 5,
+                        }}
+                    >
+                        {compatibleComponents.map((c) => {
+                            return <ComponentCard id={c.id} disabled={false} />
+                        })}
+                        {inCompatibleComponents.map((c) => {
+                            return <ComponentCard id={c.id} disabled={true} />
+                        })}
+                    </Box>
                 </Box>
             </Box>
-        </main>
+        </Box>
+    )
+}
+
+function ComponentCard({ id, disabled }) {
+    const component = components.find((c) => c.id === id)
+    return (
+        <Box
+            sx={{
+                pb: '100%',
+                position: 'relative',
+                opacity: disabled ? 0.5 : 1,
+                transition: 'all .2s ease-in-out',
+                cursor: disabled ? 'default' : 'pointer',
+                ':hover': {
+                    transform: !disabled && 'scale(1.05)',
+                },
+            }}
+        >
+            <Box
+                sx={{
+                    position: 'absolute',
+                    top: 0,
+                    bg: '#F5F5F7',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    pb: 6,
+                    px: 6,
+                    justifyContent: 'flex-end',
+                    borderRadius: '30px',
+                    right: 0,
+                    left: 0,
+                    bottom: 0,
+                }}
+            >
+                {component.image && (
+                    <Box
+                        sx={{
+                            display: 'flex',
+                            overflow: 'hidden',
+                            justifyContent: 'center',
+                            flex: 1,
+                        }}
+                    >
+                        <img src={component.image} />
+                    </Box>
+                )}
+
+                <Text sx={{ fontSize: 2, pb: 2, fontWeight: 'semibold' }}>
+                    {component.name}
+                </Text>
+                <Text
+                    sx={{
+                        fontSize: 1,
+                        color: 'fg.muted',
+                        fontWeight: 'light',
+                    }}
+                >
+                    {component.description}
+                </Text>
+            </Box>
+        </Box>
     )
 }
 
