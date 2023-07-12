740e41391a3838332c7a9b8eff79eb8c38d59d4c
Maxime De Greve <maximedegreve@github.com>
2023-07-12 19:13:41 +0100

    Add tons more examples

--- src/Exam/data.js
+++ src/Exam/data.js
@@ -6,7 +6,195 @@ import selectPanel2OneDoLight2x from './images/selectpanel2-do-light.png'
 import selectPanel2OneDoDark2x from './images/selectpanel2-do-dark.png'
 import selectPanel2OneDontLight2x from './images/selectpanel2-dont-light.png'
 import selectPanel2OneDontDark2x from './images/selectpanel2-dont-dark.png'
+
+import ActionMenu1DoLight2x from './images/actionmenu-do-light.png'
+import ActionMenu1DoDark2x from './images/actionmenu-do-dark.png'
+import ActionMenu1DontLight2x from './images/actionmenu-dont-light.png'
+import ActionMenu1DontDark2x from './images/actionmenu-dont-dark.png'
+
+import ActionMenu2DoLight2x from './images/actionmenu2-do-light.png'
+import ActionMenu2DoDark2x from './images/actionmenu2-do-dark.png'
+import ActionMenu2DontLight2x from './images/actionmenu2-dont-light.png'
+import ActionMenu2DontDark2x from './images/actionmenu2-dont-dark.png'
+
+import ActionMenu3DoLight2x from './images/actionmenu3-do-light.png'
+import ActionMenu3DoDark2x from './images/actionmenu3-do-dark.png'
+import ActionMenu3DontLight2x from './images/actionmenu3-dont-light.png'
+import ActionMenu3DontDark2x from './images/actionmenu3-dont-dark.png'
+
+import ActionMenu4DoLight2x from './images/actionmenu4-do-light.png'
+import ActionMenu4DoDark2x from './images/actionmenu4-do-dark.png'
+import ActionMenu4DontLight2x from './images/actionmenu4-dont-light.png'
+import ActionMenu4DontDark2x from './images/actionmenu4-dont-dark.png'
+
+import ActionMenu5DoLight2x from './images/actionmenu5-do-light.png'
+import ActionMenu5DoDark2x from './images/actionmenu5-do-dark.png'
+import ActionMenu5DontLight2x from './images/actionmenu5-dont-light.png'
+import ActionMenu5DontDark2x from './images/actionmenu5-dont-dark.png'
+
+import ActionMenu6DoLight2x from './images/actionmenu6-do-light.png'
+import ActionMenu6DoDark2x from './images/actionmenu6-do-dark.png'
+import ActionMenu6DontLight2x from './images/actionmenu6-dont-light.png'
+import ActionMenu6DontDark2x from './images/actionmenu6-dont-dark.png'
+
+import ActionMenu7DoLight2x from './images/actionmenu7-do-light.png'
+import ActionMenu7DoDark2x from './images/actionmenu7-do-dark.png'
+import ActionMenu7DontLight2x from './images/actionmenu7-dont-light.png'
+import ActionMenu7DontDark2x from './images/actionmenu7-dont-dark.png'
+
+import ActionMenu8DoLight2x from './images/actionmenu8-do-light.png'
+import ActionMenu8DoDark2x from './images/actionmenu8-do-dark.png'
+import ActionMenu8DontLight2x from './images/actionmenu8-dont-light.png'
+import ActionMenu8DontDark2x from './images/actionmenu8-dont-dark.png'
+
+import ActionMenu9DoLight2x from './images/actionmenu9-do-light.png'
+import ActionMenu9DoDark2x from './images/actionmenu9-do-dark.png'
+import ActionMenu9DontLight2x from './images/actionmenu9-dont-light.png'
+import ActionMenu9DontDark2x from './images/actionmenu9-dont-dark.png'
+
+import ActionMenu10DoLight2x from './images/actionmenu10-do-light.png'
+import ActionMenu10DoDark2x from './images/actionmenu10-do-dark.png'
+import ActionMenu10DontLight2x from './images/actionmenu10-dont-light.png'
+import ActionMenu10DontDark2x from './images/actionmenu10-dont-dark.png'
+
+import ActionMenu11DoLight2x from './images/actionmenu11-do-light.png'
+import ActionMenu11DoDark2x from './images/actionmenu11-do-dark.png'
+import ActionMenu11DontLight2x from './images/actionmenu11-dont-light.png'
+import ActionMenu11DontDark2x from './images/actionmenu11-dont-dark.png'
+
 const exam = [
+    {
+        id: 88,
+        component: 'action menu',
+        image: {
+            lightDo: ActionMenu1DoLight2x,
+            darkDo: ActionMenu1DoDark2x,
+            lightDont: ActionMenu1DontLight2x,
+            darkDont: ActionMenu1DontDark2x,
+        },
+        do: `Checkmark icons indicate that the dialog will close therefore the cancel or submit button isn't needed.`,
+        dont: `Checkmark icons indicate that the dialog will close therefore the cancel or submit button isn't needed.`,
+    },
+    {
+        id: 89,
+        component: 'action menu',
+        image: {
+            lightDo: ActionMenu2DoLight2x,
+            darkDo: ActionMenu2DoDark2x,
+            lightDont: ActionMenu2DontLight2x,
+            darkDont: ActionMenu2DontDark2x,
+        },
+        do: `Checkmark icons indicate that the dialog will close therefore the cancel or submit button isn't needed.`,
+        dont: `Checkmark icons indicate that the dialog will close therefore the cancel or submit button isn't needed.`,
+    },
+    {
+        id: 90,
+        component: 'action menu',
+        image: {
+            lightDo: ActionMenu3DoLight2x,
+            darkDo: ActionMenu3DoDark2x,
+            lightDont: ActionMenu3DontLight2x,
+            darkDont: ActionMenu3DontDark2x,
+        },
+        do: `Checkmark icons indicate that the dialog will close therefore the cancel or submit button isn't needed.`,
+        dont: `Checkmark icons indicate that the dialog will close therefore the cancel or submit button isn't needed.`,
+    },
+    {
+        id: 91,
+        component: 'action menu',
+        image: {
+            lightDo: ActionMenu4DoLight2x,
+            darkDo: ActionMenu4DoDark2x,
+            lightDont: ActionMenu4DontLight2x,
+            darkDont: ActionMenu4DontDark2x,
+        },
+        do: `Checkmark icons indicate that the dialog will close therefore the cancel or submit button isn't needed.`,
+        dont: `Checkmark icons indicate that the dialog will close therefore the cancel or submit button isn't needed.`,
+    },
+    {
+        id: 92,
+        component: 'action menu',
+        image: {
+            lightDo: ActionMenu5DoLight2x,
+            darkDo: ActionMenu5DoDark2x,
+            lightDont: ActionMenu5DontLight2x,
+            darkDont: ActionMenu5DontDark2x,
+        },
+        do: `Checkmark icons indicate that the dialog will close therefore the cancel or submit button isn't needed.`,
+        dont: `Checkmark icons indicate that the dialog will close therefore the cancel or submit button isn't needed.`,
+    },
+    {
+        id: 93,
+        component: 'action menu',
+        image: {
+            lightDo: ActionMenu6DoLight2x,
+            darkDo: ActionMenu6DoDark2x,
+            lightDont: ActionMenu6DontLight2x,
+            darkDont: ActionMenu6DontDark2x,
+        },
+        do: `Checkmark icons indicate that the dialog will close therefore the cancel or submit button isn't needed.`,
+        dont: `Checkmark icons indicate that the dialog will close therefore the cancel or submit button isn't needed.`,
+    },
+    {
+        id: 94,
+        component: 'action menu',
+        image: {
+            lightDo: ActionMenu7DoLight2x,
+            darkDo: ActionMenu7DoDark2x,
+            lightDont: ActionMenu7DontLight2x,
+            darkDont: ActionMenu7DontDark2x,
+        },
+        do: `Checkmark icons indicate that the dialog will close therefore the cancel or submit button isn't needed.`,
+        dont: `Checkmark icons indicate that the dialog will close therefore the cancel or submit button isn't needed.`,
+    },
+    {
+        id: 95,
+        component: 'action menu',
+        image: {
+            lightDo: ActionMenu8DoLight2x,
+            darkDo: ActionMenu8DoDark2x,
+            lightDont: ActionMenu8DontLight2x,
+            darkDont: ActionMenu8DontDark2x,
+        },
+        do: `Checkmark icons indicate that the dialog will close therefore the cancel or submit button isn't needed.`,
+        dont: `Checkmark icons indicate that the dialog will close therefore the cancel or submit button isn't needed.`,
+    },
+    {
+        id: 96,
+        component: 'action menu',
+        image: {
+            lightDo: ActionMenu9DoLight2x,
+            darkDo: ActionMenu9DoDark2x,
+            lightDont: ActionMenu9DontLight2x,
+            darkDont: ActionMenu9DontDark2x,
+        },
+        do: `Checkmark icons indicate that the dialog will close therefore the cancel or submit button isn't needed.`,
+        dont: `Checkmark icons indicate that the dialog will close therefore the cancel or submit button isn't needed.`,
+    },
+    {
+        id: 97,
+        component: 'action menu',
+        image: {
+            lightDo: ActionMenu10DoLight2x,
+            darkDo: ActionMenu10DoDark2x,
+            lightDont: ActionMenu10DontLight2x,
+            darkDont: ActionMenu10DontDark2x,
+        },
+        do: `Checkmark icons indicate that the dialog will close therefore the cancel or submit button isn't needed.`,
+        dont: `Checkmark icons indicate that the dialog will close therefore the cancel or submit button isn't needed.`,
+    },
+    {
+        id: 98,
+        component: 'action menu',
+        image: {
+            lightDo: ActionMenu11DoLight2x,
+            darkDo: ActionMenu11DoDark2x,
+            lightDont: ActionMenu11DontLight2x,
+            darkDont: ActionMenu11DontDark2x,
+        },
+        do: `Checkmark icons indicate that the dialog will close therefore the cancel or submit button isn't needed.`,
+        dont: `Checkmark icons indicate that the dialog will close therefore the cancel or submit button isn't needed.`,
+    },
     {
         id: 99,
         component: 'select panel',
@@ -28,7 +216,7 @@ const exam = [
             lightDont: selectPanelOneDontLight2x,
             darkDont: selectPanelOneDontDark2x,
         },
-        do: `2Checkmark icons indicate that the dialog will close therefore the cancel or submit button isn't needed.`,
+        do: `Checkmark icons indicate that the dialog will close therefore the cancel or submit button isn't needed.`,
         dont: `Checkmark icons indicate that the dialog will close therefore the cancel or submit button isn't needed.`,
     },
 ]
